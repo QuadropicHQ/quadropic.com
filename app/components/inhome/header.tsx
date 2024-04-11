e8414b1c288f9a94bf7b22394e4922c18a56f089
MohamedKamran <96822969+MohamedKamran@users.noreply.github.com>
2024-04-11 21:47:41 +0530

    Add files via upload

--- app/components/inhome/header.tsx
+++ app/components/inhome/header.tsx
@@ -55,7 +55,7 @@ const Header = () => {
             openNavigation ? "flex" : "hidden"
           } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
-          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+          <div className="relative z-2 flex flex-col justify-center m-auto lg:flex-row lg:bg-transparent  bg-black">
             {navigation.map((item) => (
               <a
                 key={item.id}

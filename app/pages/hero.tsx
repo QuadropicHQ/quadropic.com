8ec6f98964fb42507103a42365f2de23426a3870
Mohamed Kamran <mohamedkamran2211@icloud.com>
2024-05-20 22:43:57 +0530

    fixed dark mode and made hero page consistent

--- app/pages/hero.tsx
+++ app/pages/hero.tsx
@@ -1,22 +1,28 @@
 "use client";
-
-import { motion } from "framer-motion";
 import React from "react";
-import { AuroraBackground } from "../components/ui/aurora-background";
 import { FlipWords } from "../components/ui/flipwords";
+import { Spotlight } from "../components/ui/spotlight";
 
 export function HeroPage() {
-  const words = ["AI", "Media", "Design", "Privacy", "Technology"];
+  const words = ["AI", "Media", "Design", "Privacy", "Tech"];
 
   return (
-    <AuroraBackground>
-      <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 justify-center items-center px-4">
-        Quadropic
-      </h1>
-      <div className="text-3xl font-normal text-neutral-600 dark:text-neutral-400">
-        24th Avenue of
-        <FlipWords words={words} /> <br />
+    <div className="h-screen flex items-center justify-center">
+      <div className="w-full max-w-screen-md rounded-md flex md:items-center md:justify-center bg-black/[0.96] flex-col">
+        <Spotlight
+          className="-top-40 left-0 md:left-60 md:-top-20"
+          fill="white"
+        />
+        {/* Title Hero Section */}
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 justify-center items-center px-4">
+          Quadropic
+        </h1>
+        {/* Moto Hero Section */}
+        <div className="text-3xl font-normal mx-auto text-neutral-600 dark:text-neutral-400">
+          24th Avenue of
+          <FlipWords words={words} /> <br />
+        </div>
       </div>
-    </AuroraBackground>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ const config: Config = {
         "meteor-effect": "meteor 5s linear infinite",
         scroll:
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
-        spotlight: "spotlight 2s ease .75s 1 forwards",
         "radar-spin": "radar-spin 10s linear infinite",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
       keyframes: {
         "radar-spin": {
@@ -48,6 +48,16 @@ const config: Config = {
           "100%": {
             transform: "rotate(215deg) translateX(-500px)",
             opacity: "0",
+          },
+        },
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
           },
         },
       },

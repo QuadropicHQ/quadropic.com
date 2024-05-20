9c012e1b1ec546f4e1119471f7c8c47c0214e644
Mohamed Kamran <mohamedkamran2211@icloud.com>
2024-05-20 21:02:15 +0530

    made overflow-x issue right

--- app/pages/hero.tsx
+++ app/pages/hero.tsx
@@ -6,15 +6,15 @@ import { AuroraBackground } from "../components/ui/aurora-background";
 import { FlipWords } from "../components/ui/flipwords";
 
 export function HeroPage() {
-  const words = ["AI", "Entertainment", "Design", "Privacy", "Technology"];
+  const words = ["AI", "Media", "Design", "Privacy", "Technology"];
 
   return (
     <AuroraBackground>
-      <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+      <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 justify-center items-center px-4">
         Quadropic
       </h1>
-      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        The Twenty Fourth Avenue of
+      <div className="text-3xl font-normal text-neutral-600 dark:text-neutral-400">
+        24th Avenue of
         <FlipWords words={words} /> <br />
       </div>
     </AuroraBackground>

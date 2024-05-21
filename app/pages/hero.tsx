6249a2bc3edf3af304595a224f8a3337c62817af
Mohamed Kamran <mohamedkamran2211@icloud.com>
2024-05-22 00:07:53 +0530

    chore: Update HeroPage layout and add Vortex component

--- app/pages/hero.tsx
+++ app/pages/hero.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { FlipWords } from "../components/ui/flipwords";
 import { Spotlight } from "../components/ui/spotlight";
 import QuadroBanner from "../components/inhome/quadro-text";
+import { Vortex } from "../components/ui/vortex";
 
 export function HeroPage() {
   const words = ["AI", "Media", "Design", "Privacy", "Tech"];
@@ -10,14 +11,16 @@ export function HeroPage() {
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="w-full max-w-screen-md rounded-md flex md:items-center md:justify-center bg-black/[0.96] flex-col">
+        <Vortex
+          backgroundColor="black"
+          className="flex items-center flex-col justify-center px-2 md:px-10 py-4"
+        />
         <Spotlight
           className="-top-40 left-0 md:left-60 md:-top-20"
           fill="white"
         />
-        {/* Title Hero Section */}
         <QuadroBanner />
-        {/* Moto Hero Section */}
-        <div className="text-3xl font-normal mx-auto text-neutral-600 dark:text-neutral-400">
+        <div className="text-3xl font-normal mx-auto text-neutral-600 dark:text-neutral-400 z-10">
           24th Avenue of
           <FlipWords words={words} /> <br />
         </div>

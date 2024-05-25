b5d65fd741f67fff38f3811a322a4272e519be16
Mohamed Kamran <mohamedkamran2211@icloud.com>
2024-05-25 22:11:50 +0530

    chore: Update HeroPage layout and optimize performance

--- app/pages/hero.tsx
+++ app/pages/hero.tsx
@@ -1,39 +1,35 @@
-"use client";
-import React from "react";
-import { FlipWords } from "../components/ui/flipwords";
 import { Spotlight } from "../components/ui/spotlight";
 import QuadroBanner from "../components/inhome/quadrobanner";
-import { AuroraBackground } from "../components/ui/aurora-background";
-import { motion } from "framer-motion";
+import { FlipWords } from "../components/ui/flipwords";
 
 export function HeroPage() {
   const words = ["AI", "Media", "Design", "Privacy", "Tech"];
 
   return (
-    <AuroraBackground>
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="relative flex flex-col gap-4 items-center justify-center px-4"
-      >
+    <div className="h-screen flex items-center justify-center">
+      <div className="w-full max-w-screen-md rounded-md flex md:items-center md:justify-center bg-black/[0.96] flex-col">
         <Spotlight
           className="-top-40 left-0 md:left-60 md:-top-20"
+          fill="blue"
+        />
+        <Spotlight
+          className="-top-20 left-0 md:left-40 md:-top-10 sm:left-200 sm:-top-10"
+          fill="lightblue"
+        />
+        <Spotlight
+          className="-top-10 left-0 md:left-20 md:-top-5 sm:left-180 sm:-top-20"
+          fill="darkblue"
+        />
+        <Spotlight
+          className="-top-40 left-0 md:left-80 md:-top-30 sm:left-160 sm:-top-40"
           fill="white"
         />
-
         <QuadroBanner />
         <div className="text-3xl font-normal mx-auto text-neutral-600 dark:text-neutral-400 z-10">
-          <h1 className="text-2xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-            24th Avenue of
-            <FlipWords words={words} /> <br />
-          </h1>
+          24th Avenue of
+          <FlipWords words={words} /> <br />
         </div>
-      </motion.div>
-    </AuroraBackground>
+      </div>
+    </div>
   );
 }

fe3aafa258b3ada9c5fd609b031401e56a5bf92f
Mohamed Kamran <mohamedkamran2211@icloud.com>
2024-05-30 18:25:10 +0530

    made template for service section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { B2BservicesSection } from "./pages/b2bservices";
 import { HeroPage } from "./pages/hero";
 import { TestimonialsSection } from "./pages/testimonials";
-import { GeneralServices } from "./pages/gservices";
 import Header from "./components/inhome/header";
 import { Footer } from "./components/inhome/footer";
 import { ValuePage } from "./pages/values";
 import Head from "next/head";
+import { GServices } from "./pages/gservices";
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
       <Header />
       <HeroPage />
       <ValuePage />
-      <GeneralServices />
+      <GServices />
       <B2BservicesSection />
       <TestimonialsSection />
       <Footer />

e3d5b692635632104d1b3fb7a84f30654bd94949
Mohamed Kamran <kamran@quadropic.com>
2024-12-30 16:06:00 +0530

    added `analytics` and `performance` using the `Firebase SDK` and removed the automed `GitHub Actions` `perf` in `nextjs.yml`

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getPerformance } from "firebase/performance";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,5 +18,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const perf = getPerformance(app);
+const analytics = getAnalytics(app);
 
-export { db };
+export { db , perf, analytics};

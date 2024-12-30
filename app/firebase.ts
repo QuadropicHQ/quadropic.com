e05d72fb375f04247b258c239b9e0bfef00135cb
Mohamed Kamran <kamran@quadropic.com>
2024-12-30 15:50:18 +0530

    `critical security` fix Fix : Exposed Firebase Secrets were transferred to `.env` file

--- app/firebase.ts
+++ app/firebase.ts
@@ -6,14 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDR_K_MMHKVw7zW5cRZrhpJnHZ6T__z0Vg",
-  authDomain: "contact-quadropicdotcom.firebaseapp.com",
-  projectId: "contact-quadropicdotcom",
-  storageBucket: "contact-quadropicdotcom.firebasestorage.app",
-  messagingSenderId: "777940702134",
-  appId: "1:777940702134:web:62763eacb428882c242717"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);

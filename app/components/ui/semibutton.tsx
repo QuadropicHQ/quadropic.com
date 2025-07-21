984af9063e94fa00270d43b9c5283a288db2be76
MohamedKamran <kamran@quadropic.com>
2025-07-21 23:55:09 +0530

    feat: update SemiButton to accept href prop and link to scheduling page

--- app/components/ui/semibutton.tsx
+++ app/components/ui/semibutton.tsx
@@ -1,11 +1,12 @@
 interface SemiButtonProps {
+  href?: string;
     children?: React.ReactNode;
     className?: string;
 }
 
-export default function SemiButton({ children = "Coming Soon", className }: SemiButtonProps) {
+export default function SemiButton({ children = "Schedule a Call to know more", className, href }: SemiButtonProps) {
     return (
-        <button className={`bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block ${className}`}>
+        <a href={href || '#'} target="_blank" rel="noopener noreferrer" className={`bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block ${className}`}>
             <span className="absolute inset-0 overflow-hidden rounded-full">
             <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(255,255,255,0.6)_0%,rgba(255,255,255,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
             </span>
@@ -13,6 +14,6 @@ export default function SemiButton({ children = "Coming Soon", className }: Semi
             <span>{children}</span>
             </div>
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-white/0 via-white/90 to-white/0 transition-opacity duration-500 group-hover:opacity-40" />
-        </button>
+        </a>
     );
 }
